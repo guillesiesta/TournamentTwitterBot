ee1c1b89ea8a54952e0763fdc0b08202ae8308fc
guillesiesta <guille.siesta.muriel@gmail.com>
2019-09-20 23:01:29 +0200

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,35 @@
+from yattag import Doc
+import imgkit
+
+f = open("web.html", "w")
+
+doc, tag, text, line = Doc(
+    defaults = {'ingredient': ['chocolate', 'coffee']}
+).ttl()
+
+with tag('h1'):
+    text('Hello world!')
+    text('Hello world!')
+
+with tag('h2'):
+    text('Caca pedo y culo')
+
+
+with tag('form', action = ""):
+    line('label', 'Select one or more ingredients')
+    with doc.select(name = 'ingredient', multiple = "multiple"):
+        for value, description in (
+            ("chocolate", "Dark Chocolate"),
+            ("almonds", "Roasted almonds"),
+            ("honey", "Acacia honey"),
+            ("coffee", "Ethiopian coffee")
+        ):
+            with doc.option(value = value):
+                text(description)
+    doc.stag('input', type = "submit", value = "Validate")
+
+f.write(doc.getvalue())
+
+f.close()
+
+imgkit.from_file('web.html','out.jpg')
