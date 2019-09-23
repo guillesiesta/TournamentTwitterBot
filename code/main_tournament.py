cf14712cf267833303db50d9faa628adb6385eb1
guillesiesta <guille.siesta.muriel@gmail.com>
2019-09-24 00:57:54 +0200

    save result into json

--- code/main_tournament.py
+++ code/main_tournament.py
@@ -1,6 +1,7 @@
 from randomdict import shuffleDict
 from random import choice
 import time
+import json
 # create list of players
 lista_jugadores = {
     "lusesitas":"quesadilla",
@@ -12,11 +13,10 @@ lista_jugadores = {
     "paco":"pelota",
     "pablo":"aburrimiento"}
 
+torneo = {"BattleRoyale":{}}
 
-
-def eliminatorias(lista_jugadores):
+def eliminatorias(lista_jugadores,torneo,ronda):
     lista_jugadores_eliminados = []
-
     if(len(lista_jugadores)==1):
         print("El ganador es "+str(list(lista_jugadores)))
         return ""
@@ -24,25 +24,36 @@ def eliminatorias(lista_jugadores):
     if(len(lista_jugadores)==2):
         print("Llega la gran final!")
 
+
+    torneo["BattleRoyale"]["ronda"+str(ronda)]= {}
+    lucha=0
+
     for i in range(0,int((len(lista_jugadores))),2):
-        iteracion = 0
         local = list(lista_jugadores)[i]
         visitante = list(lista_jugadores)[i+1]
         jugador_eliminado = choice([local,visitante])
         jugador_eliminado = choice([local,visitante])
         jugador_eliminado = choice([local,visitante])
         lista_jugadores_eliminados.append(jugador_eliminado)
+
+        torneo["BattleRoyale"]["ronda"+str(ronda)]["lucha"+str(lucha)]={local:lista_jugadores.get(local,None),visitante:lista_jugadores.get(visitante,None)}
+        with open("file.json", "wb") as f:
+            f.write(json.dumps(torneo).encode("utf-8"))
+
         if(jugador_eliminado==local):
             print(visitante+" ha asesinado a "+local+" usando "+lista_jugadores.get(visitante,None))
+            torneo["BattleRoyale"]["ronda"+str(ronda)]["lucha"+str(lucha)]={local:lista_jugadores.get(local,None),visitante:lista_jugadores.get(visitante,None), "process":True}
             #generarJSON
             #capturar imagen de website
             #escribir tuit
         else:
             print(local+" ha asesinado a "+visitante+" usando "+lista_jugadores.get(local,None))
+            torneo["BattleRoyale"]["ronda"+str(ronda)]["lucha"+str(lucha)]={local:lista_jugadores.get(local,None),visitante:lista_jugadores.get(visitante,None), "process":True}
             #generarJSON
             #capturar imagen de website
             #escribir tuit
-        time.sleep(2)
+        lucha = lucha + 1
+        #time.sleep(2)
 
     print("Los jugadores "+str(lista_jugadores_eliminados)+" han sido eliminados. Mala suerte. Comienza la siguiente ronda.")
     #eliminar de la lista_jugadores a los jugadores lista_jugadores_eliminados
@@ -51,7 +62,8 @@ def eliminatorias(lista_jugadores):
 
     print("Los jugadores "+str(list(lista_jugadores))+" pasan a la siguiente ronda. Enhorabuena.")
 
-    return(eliminatorias(lista_jugadores))
+    ronda=ronda+1
+    return(eliminatorias(lista_jugadores,torneo,ronda))
 
 
 #shuffle lista_jugadores
@@ -59,11 +71,9 @@ lista_jugadores = shuffleDict(lista_jugadores)
 
 print(list(lista_jugadores))
 
-eliminatorias(lista_jugadores)
+ronda = 0
 
-
-
-
+eliminatorias(lista_jugadores,torneo,ronda)
 
 #print(choice(list(lista_jugadores.keys())))
 
