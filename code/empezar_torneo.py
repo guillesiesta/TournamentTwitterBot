cc8ea4b501e2660619aedf54bea912c0d98ac71a
WUEGMU03 <guillermo.murielsanchez-lafuente@altran.com>
2019-10-17 15:20:01 +0200

    scheduler creado para el lanzamiento del torneo, y añadida request que hace post al front

---
+++ code/empezar_torneo.py
@@ -0,0 +1,24 @@
+import schedule
+import time
+import requests
+import json
+from json_operations import cargarJson
+
+n=1
+
+def takeJsonTournament(id):
+    json = cargarJson("jsons/dans_test_"+str(id)+".json")
+    return json
+
+def job():
+    global n
+    json_t = takeJsonTournament(n)
+    r = requests.post("http://hkr.es/services/updater.php",data=json.dumps(json_t), headers={'Content-Type': 'application/json'})
+    print("Request done... "+str(n))
+    n=n+1
+    
+
+schedule.every(0.1).minutes.do(job)
+
+while True:
+    schedule.run_pending()
