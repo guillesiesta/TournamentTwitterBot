af7092d4f7110d75e9503ff761834a53e9d6b13e
guillesiesta <guille.siesta.muriel@gmail.com>
2019-10-18 16:54:49 +0200

    añadida parte en la que se hace captura de la web

--- code/empezar_torneo.py
+++ code/empezar_torneo.py
@@ -2,6 +2,7 @@ import schedule
 import time
 import requests
 import json
+import imgkit
 from json_operations import cargarJson
 from main_tournament import start
 
@@ -17,8 +18,15 @@ def takeJsonTournament(id):
 def job():
     global n
     json_t = takeJsonTournament(n)
+    # actualizar web con json
     r = requests.post("http://hkr.es/services/updater.php",data=json.dumps(json_t), headers={'Content-Type': 'application/json'})
     print("Request done... "+str(n))
+    time.sleep(10)
+    # capturar torneo web
+    imgkit.from_url('http://www.hkr.es/', 'imgs/hkr'+str(n)+'.jpg')
+    print("Capture done... "+str(n))
+    time.sleep(10)
+    #crear tweet y enviar
     n=n+1
 
 
