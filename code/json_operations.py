d0a9f1413b0eaabde6e002f3967576b0a69fb7c5
tiws <tiws.future@gmail.com>
2020-01-09 12:34:55 +0100

    al empezar siempre con rondas vacias, este archivo sera el 0, por lo que no hace falta sumar

--- code/json_operations.py
+++ code/json_operations.py
@@ -14,5 +14,7 @@ def guardarJson(contenido):
     path = 'jsons/'
     num_files = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
     #print(num_files)
-    with open("jsons/dans_test_"+str(num_files+1)+".json", "w") as f:
+    #antes ponia num_files +1 para ir incrementando el id del json
+    #ahora al empezar desde 0 cuando se hace num_files no hace falta sumarle 1
+    with open("jsons/dans_test_"+str(num_files)+".json", "w") as f:
         json.dump(contenido, f)
